d78b1ea0cdcf19f49e18148a97be7860748142fe
S-v-3-n <sven.mrs.n@gmail.com>
2026-02-01 23:18:39 +0100

    setup turbo stream update

---
+++ assets/controllers/message_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from '@hotwired/stimulus';
+
+/*
+ * This is an example Stimulus controller!
+ *
+ * Any element with a data-controller="hello" attribute will cause
+ * this controller to be executed. The name "hello" comes from the filename:
+ * hello_controller.js -> "hello"
+ *
+ * Delete this file or adapt it for your use!
+ */
+export default class extends Controller {
+    connect() {
+
+        console.log('messageController');
+
+    }
+
+
+
+    valid(){
+        console.log('validate');
+        document.querySelector('#message_form').requestSubmit();
+    }
+}
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Form\MessageType;
+use Symfony\UX\Turbo\TurboBundle;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,20 +11,49 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class HomeController extends AbstractController
 {
-
-    #[Route('/', name: 'app_home')]
+    #[Route('/home', name: 'app_home', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        $form = $this->createForm(MessageType::class);
+        return $this->render('home/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
-    #[Route('/message', name: 'app_message', methods: ['POST'])]
-    public function message(Request $request): Response
-    {
-        $message = $request->request->get('message');
 
-        return $this->render('home/_message.stream.html.twig', [
-            'message' => $message,
+    #[Route('/', name: 'app_message', methods: ['GET', 'POST'])]
+    public function new(Request $request): Response
+    {
+        $form = $this->createForm(MessageType::class);
+
+        $form->handleRequest($request);
+
+        dump('yooo');
+        dump($form->isSubmitted() && $form->isValid());
+        dump($request->getRequestFormat());
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $messages = $form->getData();
+
+            // die;
+            // if ($request->getRequestFormat() === TurboBundle::STREAM_FORMAT) {
+                // If the request comes from Turbo, set the content type as text/vnd.turbo-stream.html and only send the HTML to update
+                $request->setRequestFormat(TurboBundle::STREAM_FORMAT);
+                return $this->renderBlock('home/index.html.twig', 'message_stream',
+                [
+                    'messages' => $messages,
+                    'form' => $form,
+                ]);
+            // }
+
+
+            // If the client doesn't support JavaScript, or isn't using Turbo, the form still works as usual.
+            // Symfony UX Turbo is all about progressively enhancing your applications!
+        }
+
+        return $this->render('home/index.html.twig', [
+            'form' => $form,
+            'messages' => []
         ]);
-}
+    }
 }
